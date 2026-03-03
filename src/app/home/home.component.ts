467eeea52f52e7973cfc33bed42bd2cb24e5b49b
Gabriel Fernando Correa <gabrielfcorrea3@gmail.com>
2026-03-03 20:10:02 -0300

    agrego botones a la vista home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,      // 👈 IMPORTANTE
+    MatButtonModule
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent {}
 
-}
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-home',
+//   imports: [],
+//   templateUrl: './home.component.html',
+//   styleUrl: './home.component.css'
+// })
+// export class HomeComponent {
+
+// }
